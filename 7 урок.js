d25ba48572b1c91b7167ff0334c5242065213727
StellIris66 <timoxa15072010@gmail.com>
2024-11-13 20:00:39 +0500

    mission JS 8 imvddd

--- 7 урок.js
+++ 7 урок.js
@@ -1,5 +1,22 @@
-function cleatDiv() {
+function clearDiv() {
+        console.log('вызываем очистку дива');
+        // получаем див для очистки
         let div = document.getElementById('clear');
+        // чистим всё то, что внутри
         div.innerText = '';
+}
 
+function makeRed() {
+        let body = document.getElementById('sdf');
+        body.style.backgroundColor = 'red';
+}
+
+function makeGreen() {
+        let body = document.getElementById('sdf');
+        body.style.backgroundColor = 'green';
+}
+
+function makeBlue() {
+        let body = document.getElementById('sdf');
+        body.style.backgroundColor = 'blue';
 }
---
+++ 8 занятие 1 задача.js
@@ -0,0 +1,24 @@
+function makeRed() {
+    let body = document.getElementById('body');
+    body.style.backgroundColor = 'red';
+}
+
+function makeGreen() {
+    let body = document.getElementById('body');
+    body.style.backgroundColor = 'green';
+}
+
+function makeBlue() {
+    let body = document.getElementById('body');
+    body.style.backgroundColor = 'blue';
+}
+
+function makeaqua() {
+    let body = document.getElementById('body');
+    body.style.backgroundColor = 'aqua';
+}
+
+function makemediumaquamarine() {
+    let body = document.getElementById('body');
+    body.style.backgroundColor = 'mediumaquamarine';
+}
