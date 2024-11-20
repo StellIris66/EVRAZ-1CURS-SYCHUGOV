ce020981d784094e3284e57ecded6fc066692ea6
StellIris66 <timoxa15072010@gmail.com>
2024-11-20 19:58:10 +0500

    mission JS 8 Alert 33333333333333333333333333333333

---
+++ js work 4.js
@@ -0,0 +1,18 @@
+function clearVse() {
+    console.log('очистка всего');
+    let Vse = document.getElementById('clear');
+    Vse.innerHTML = '';
+}
+function kn1() {
+    console.log('перенос текста')
+    let d1 = document.getElementById('d1');
+
+}
+
+function kn2() {
+
+}
+
+function kn3() {
+
+}
---
+++ работа 2 8 занятие.js
@@ -0,0 +1,5 @@
+function changeText() {
+    let change= prompt("Enter text here: ");
+    let div = document.getElementById("change");
+    div.innerText = change;
+}
