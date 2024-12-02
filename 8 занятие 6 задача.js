5102304fe875ced1c6c4a19fe79462bd4ce5aca9
StellIris66 <timoxa15072010@gmail.com>
2024-12-02 18:01:03 +0500

    Продолжение работы над 6 задачей(все еще 8 урок)))))))))))))))))))))))))))))))

---
+++ 8 занятие 6 задача.js
@@ -0,0 +1,17 @@
+function kn1(){
+    let ggg = prompt('Напиши какой по порядку блок(1,2,3)')
+    let input1 = document.getElementById("d1");
+    let input2 = document.getElementById("d1");
+    let input3 = document.getElementById("d1)");
+    console.log(ggg.innerText);
+
+
+
+}
+
+
+function clearButton(){
+    span1.innerText = '';
+    span2.innerText = '';
+    span3.innerText = '';
+}
---
+++ 9 занятие 1.js
@@ -0,0 +1,51 @@
+//let a = 10;
+//let b = 15;
+//if (a>b) {
+//    console.log('а больше b');
+//}
+//else {
+//    console.log('а равна или   меньше b')
+//}
+//
+//console.log('лог больше if')
+//
+//let a1 = 9;
+//if (a1 % 2 ==
+function nolick() {
+    let back = document.getElementById('back');
+    body.style.backgroundColor = 'black';
+    body.style.textcolor = 'white';
+
+}
+function odin() {
+    let back1 = document.getElementById('back1');
+    body.style.backgroundColor = 'white';
+    body.style.textcolor = 'black';
+
+
+}
+function dwa() {
+    let back2 = document.getElementById('back2');
+    body.style.backgroundColor = 'blue';
+    body.style.textcolor = 'white';
+
+
+}
+function five() {
+    let back5 = document.getElementById('back5');
+    body.style.backgroundColor = 'green';
+    body.style.textcolor = 'white';
+
+
+}
+function plus1() {
+
+
+
+}
+function minus1() {
+
+
+
+
+}
