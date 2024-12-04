8823888d758b80466484c6dc9a34d62f092312d9
StellIris66 <timoxa15072010@gmail.com>
2024-12-04 19:54:09 +0500

    Продолжение работы над условиями 1задача (85%)

--- 9 занятие 1.js
+++ 9 занятие 1.js
@@ -11,41 +11,79 @@
 //
 //let a1 = 9;
 //if (a1 % 2 ==
-function nolick() {
-    let back = document.getElementById('back');
-    body.style.backgroundColor = 'black';
-    body.style.textcolor = 'white';
+
+let a0 = 0;
+
+function nolick(){
+    a0.value = 0;
+
+    let sdf = document.getElementById("sdf");
+    sdf.style.backgroundColor = "black";
+    sdf.style.color = "white";
 
 }
-function odin() {
-    let back1 = document.getElementById('back1');
-    body.style.backgroundColor = 'white';
-    body.style.textcolor = 'black';
 
-
-}
-function dwa() {
-    let back2 = document.getElementById('back2');
-    body.style.backgroundColor = 'blue';
-    body.style.textcolor = 'white';
-
+function odin(){
+    a0.value = 1;
+    let sdf = document.getElementById("sdf");
+    sdf.style.backgroundColor = "white";
+    sdf.style.color = "black";
 
 }
-function five() {
-    let back5 = document.getElementById('back5');
-    body.style.backgroundColor = 'green';
-    body.style.textcolor = 'white';
 
+
+
+
+function dwa(){
+    a0.value = 2;
+    let sdf = document.getElementById("sdf");
+    sdf.style.backgroundColor = "blue";
+    sdf.style.color = "white";
+}
+
+function five(){
+    a0.value = 5;
+    let sdf = document.getElementById("sdf");
+    sdf.style.backgroundColor = "green";
+    sdf.style.color = "white";
+}
+
+function plus1(){
+    a0.value = a0 + 1;
+    let sdf = document.getElementById("sdf");
+    if (a0 % 2){
+        function color0(){
+            let sdf = document.getElementById("sdf");
+            sdf.style.backgroundColor = "red";
+            sdf.style.color = "black";
+        }
+    }
+    else{
+        let sdf = document.getElementById("sdf");
+        sdf.style.backgroundColor = "gold";
+        sdf.style.color = "black";
+    }
 
 }
-function plus1() {
+function minus1(){
+    a0.value = a0-1;
+    let sdf = document.getElementById("sdf");
+    if (a0 % 2){
+        function color0(){
+            let sdf = document.getElementById("sdf");
+            sdf.style.backgroundColor = "red";
+            sdf.style.color = "black";
+        }
+    }
+    else{
+        let sdf = document.getElementById("sdf");
+        sdf.style.backgroundColor = "gold";
+        sdf.style.color ="black";
 
-
+    }
+    changeText.innerText = a0;
 
 }
-function minus1() {
 
-
-
-
-}
+let changeText = document.getElementById("total");
+changeText.innerText = a0;
