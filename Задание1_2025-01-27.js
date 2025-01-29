067209d1c735af3f1173c517a426b6257b3d71d9
StellIris66 <timoxa15072010@gmail.com>
2025-01-29 20:00:04 +0500

    верстали 5 (15 минут)
    Новая тема, new task 1,2,3
    
    Work to home
    2222222222222222222222222222222

--- Задание1_2025-01-27.js
+++ Задание1_2025-01-27.js
@@ -1,4 +1,19 @@
-function addOne(){
-    let n = document.getElementById()
+function addOne() {
+    let n = document.getElementById('listNames')
+    let inp1 = document.getElementById('vvodone')
+    let liElement = document.createElement('li')
+    if (inp1.value === '') {
+        alert('Добавь значение!')
+    }
+    else {
+        liElement.innerText = inp1.value;
+        n.append(liElement);
+        inp1.value = '';
+    }
+}
+
+function addTwo(){
+    let sp = document.getElementById('liseSities');
+    let inp2 = document.getElementById('vvodtwo');
 
 }
