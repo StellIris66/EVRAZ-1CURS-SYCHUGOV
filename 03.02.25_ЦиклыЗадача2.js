385825c9de52284edaf26727c07f46a73bd2511d
StellIris66 <timoxa15072010@gmail.com>
2025-02-05 20:00:40 +0500

    верстали 5 (15 минут)
    Новая тема, new task 1,2,3
    
    Work to home
    2222222222222222222222222222222

--- 03.02.25_ЦиклыЗадача2.js
+++ 03.02.25_ЦиклыЗадача2.js
@@ -1,3 +1,25 @@
-function dobav1(){
-    let
+function dobav1() {
+    let names = document.getElementById('NameList')
+    let ione = document.getElementById('inp1')
+    let jone = document.getElementById('mnoj1')
+    let liElement = document.createElement('li')
+    if (ione.value === '') {
+        alert('Внесите данные!')
+    }
+    else if (jone.value === '') {
+        alert('Внесите значение!')
+    }
+    else {
+        for(let i = 0; i<jone.value; i++);
+        i.value = ione.value;
+        liElement.innerText = ione.value;
+
+
+    }
 }
+
+//for(let i = 0; i<5; i++) <----- подсказка
+
+//        liElement.innerText = inp2.value;
+//         sp.append(liElement);
+//         inp2.value = '';
