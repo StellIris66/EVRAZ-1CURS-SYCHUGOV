4fd5b124ae847d8b490b9872a791391ce3d25b2a
StellIris66 <timoxa15072010@gmail.com>
2025-02-12 20:01:27 +0500

    верстали 5 (15 минут)
    Новая тема, new task 1,2,3
    
    Work to home
    2222222222222222222222222222222

--- 03.02.25_ЦиклыЗадача2.js
+++ 03.02.25_ЦиклыЗадача2.js
@@ -2,7 +2,6 @@ function dobav1() {
     let names = document.getElementById('NameList')
     let ione = document.getElementById('inp1')
     let jone = document.getElementById('mnoj1')
-    let liElement = document.createElement('li')
     if (ione.value === '') {
         alert('Внесите данные!')
     }
@@ -10,16 +9,35 @@ function dobav1() {
         alert('Внесите значение!')
     }
     else {
-        for(let i = 0; i<jone.value; i++);
-        i.value = ione.value;
-        liElement.innerText = ione.value;
-
-
+        for (let i = 0; i < jone.value; i++) {
+            let liElement = document.createElement('li')
+            liElement.innerText = ione.value;
+            names.append(liElement)
+        }
+        jone.value = '';
+        ione.value = '';
     }
 }
 
-//for(let i = 0; i<5; i++) <----- подсказка
+function dobav2() {
+    let names2 = document.getElementById('NameSities')
+    let ione2 = document.getElementById('inp2')
+    let jone2 = document.getElementById('mnoj2')
+    let liElement = document.createElement('li')
+    if (ione2.value === '') {
+        alert('Внесите данные!')
+    }
+    else if (jone2.value === '') {
+        alert('Внесите значение!')
+    }
+    else {
+        for (let i = 0; i < jone2.value; i++) {
+            let liElement = document.createElement('li')
+            liElement.innerText = ione2.value;
+            names2.append(liElement)
+        }
+        jone2.value = '';
+        ione2.value = '';
 
-//        liElement.innerText = inp2.value;
-//         sp.append(liElement);
-//         inp2.value = '';
+    }
+}
---
+++ 12.02.25_Задание3.js
@@ -0,0 +1,29 @@
+function butt1() {
+    let spn1 = document.getElementById('sp1')
+    let i = '0 1 2 3 4'
+
+}
+
+function butt2() {
+    let spn2 = document.getElementById('sp2')
+}
+
+function butt3() {
+    let spn3 = document.getElementById('sp3')
+}
+
+function butt4() {
+    let spn4 = document.getElementById('sp4')
+}
+
+function butt5() {
+    let spn5 = document.getElementById('sp5')
+}
+
+function butt6() {
+    let spn6 = document.getElementById('sp6')
+}
+
+function butt7() {
+    let spn7 = document.getElementById('sp7')
+}
