117c2101a251f7dc084ab73f2013e849cda75a89
StellIris66 <timoxa15072010@gmail.com>
2025-02-17 18:00:39 +0500

    ereoiejgi0rjj5t545477

--- 12.02.25_Задание3.js
+++ 12.02.25_Задание3.js
@@ -1,29 +1,46 @@
 function butt1() {
     let spn1 = document.getElementById('sp1')
-    let i = '0 1 2 3 4'
 
+    for (let i = 0;i<5; i++){
+        spn1.innerText = spn1.innerText + ' ' + i;
+
+    }
 }
 
 function butt2() {
     let spn2 = document.getElementById('sp2')
+    for (let i = 1;i<=5; i++) {
+        spn2.innerText = spn2.innerText + ' ' + i;
+    }
 }
 
 function butt3() {
     let spn3 = document.getElementById('sp3')
+    for(let i = 0;i < 10; i++){
+        spn3.innerText = spn3.innerText + ' ' +i;
+    }
 }
 
 function butt4() {
     let spn4 = document.getElementById('sp4')
+    for(let i = 1;i <= 10; i++){
+        spn4.innerText = spn4.innerText + ' ' +i;
+    }
 }
 
 function butt5() {
     let spn5 = document.getElementById('sp5')
+    for(let i = 1;i % 2 <= 10; i++){
+        spn5.innerText = spn5.innerText + ' ' +i;
+    }
 }
 
 function butt6() {
     let spn6 = document.getElementById('sp6')
+    let i = 1;
 }
 
 function butt7() {
     let spn7 = document.getElementById('sp7')
-}
+    let i = 1;
+    }
