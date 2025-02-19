21955a68ae37c3602dfc2ef440fc4753743fc2a2
StellIris66 <timoxa15072010@gmail.com>
2025-02-19 19:59:28 +0500

    ereoiejgi0rjj5t545477

--- 12.02.25_Задание3.js
+++ 12.02.25_Задание3.js
@@ -1,7 +1,6 @@
 function butt1() {
     let spn1 = document.getElementById('sp1')
-
-    for (let i = 0;i<5; i++){
+    for (let i = 0; i < 5; i++) {
         spn1.innerText = spn1.innerText + ' ' + i;
 
     }
@@ -9,38 +8,51 @@ function butt1() {
 
 function butt2() {
     let spn2 = document.getElementById('sp2')
-    for (let i = 1;i<=5; i++) {
+    for (let i = 1; i <= 5; i++) {
         spn2.innerText = spn2.innerText + ' ' + i;
     }
 }
 
 function butt3() {
     let spn3 = document.getElementById('sp3')
-    for(let i = 0;i < 10; i++){
-        spn3.innerText = spn3.innerText + ' ' +i;
+    for (let i = 0; i < 10; i++) {
+        spn3.innerText = spn3.innerText + ' ' + i;
     }
 }
 
 function butt4() {
     let spn4 = document.getElementById('sp4')
-    for(let i = 1;i <= 10; i++){
-        spn4.innerText = spn4.innerText + ' ' +i;
+    for (let i = 1; i <= 10; i++) {
+        spn4.innerText = spn4.innerText + ' ' + i;
     }
 }
 
 function butt5() {
     let spn5 = document.getElementById('sp5')
-    for(let i = 1;i % 2 <= 10; i++){
-        spn5.innerText = spn5.innerText + ' ' +i;
+    for (let i = 0; i <= 18; i+=2) {
+        spn5.innerText = spn5.innerText + ' ' + i;
     }
 }
 
 function butt6() {
     let spn6 = document.getElementById('sp6')
-    let i = 1;
+    for (let i = 1; i <= 19; i+=2) {
+        spn6.innerText = spn6.innerText + ' ' + i;
+    }
 }
 
 function butt7() {
     let spn7 = document.getElementById('sp7')
-    let i = 1;
+
+    for (let i = 1; i <= 20; i++) {
+        let span = document.createElement('span')
+        span.innerText = i + ' ';
+
+        if (i % 2 === 0) {
+            span.style.color = 'red';
+        }
+
+        spn7.append(span)
     }
+
+}
