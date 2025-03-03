aac24092bd68dc0edc8b4166412e59c76cdd3431
StellIris66 <timoxa15072010@gmail.com>
2025-03-03 18:01:34 +0500

    ereoiejgi0rjj5t545477

--- 24.02.25_Задание44.js
+++ 24.02.25_Задание44.js
@@ -1,24 +1,26 @@
 Jame()
 
 function Jame() {
-    let xentrioN = prompt('1-red,2-green, 3-blue, 4-stop');
+
     // let li =document.createElement('li');
     let io = document.getElementById('spisk');
-    console.log(xentrioN)
-    while (xentrioN >= 4) {
+    while (true) {
+        let xentrioN = prompt('1-red,2-green, 3-blue, 4-stop');
         let div = document.createElement('div');
-        if (xentrioN === 1) {
+        if (xentrioN == 1) {
             div.classList.add('blockred')
             io.append(div)
         }
-        else if (xentrioN === 2) {
+        else if (xentrioN == 2) {
             div.classList.add('blockgreen')
             io.append(div)
         }
-        else {
+        else if (xentrioN == 3){
             div.classList.add('blockblue')
             io.append(div)
         }
-
+        else{
+            break;
+        }
     }
 }
---
+++ 26.02.25_Задание5.js
@@ -0,0 +1,4 @@
+function nums(){
+    let UlElement = document.createElement('ul')
+    let inp1 = document.getElementById()
+}
