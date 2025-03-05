ed24854b2fb8e580ae52a4d77cc5aa26dec6f920
StellIris66 <timoxa15072010@gmail.com>
2025-03-05 20:00:30 +0500

    фисташкэ

--- 26.02.25_Задание5.js
+++ 26.02.25_Задание5.js
@@ -1,4 +1,18 @@
+function dobav(){
+    let inp1 = document.getElementById('in');
+    let doel = document.getElementById('dobel');
+    let a = inp1.innerText;
+    doel.innerText = a.innerText
+}
+
 function nums(){
     let UlElement = document.createElement('ul')
     let inp1 = document.getElementById()
+}
+
+function nenums(){
+    let OlElement = document.createElement('ol')
+}
+
+function clears() {
 }
